ba999afc06d0c49c8ffb656b2158e815c579d8c1
ManojNambiraj <postboxmano@gmail.com>
2025-06-17 19:05:37 +0530

    first commit

---
+++ class2/variables.py
@@ -0,0 +1,19 @@
+# Output Statements:
+
+    # print("Hello, Good evening...!", end=" ")   # End Statement
+    # print("Good night")
+
+    # print("09", "11", "1995", sep="-")          # Seperate Statement
+
+# Variables:
+
+    # myAgeInMySchoolDays = 20
+
+    # print(myAgeInMySchoolDays)
+
+# Dataypes
+
+a = None
+
+print(a)
+print(type(a))
