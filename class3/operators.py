024a797073039c8deb9e9f0f91003235debc25af
ManojNambiraj <postboxmano@gmail.com>
2025-06-17 19:21:23 +0530

    Operators

---
+++ class3/operators.py
@@ -0,0 +1,27 @@
+# Operators:
+
+"""
+    Arithmetic operators
+        (+, -, *, /, %, **, //)
+
+    Assignment operators
+    Comparison operators
+    Logical operators
+    Identity operators
+    Membership operators
+"""
+
+# Arithmetic operators  --> (+, -, *, /, %, **, //)
+
+a = 10
+b = 3
+
+# result = a + b
+# result = a - b
+# result = a * b
+# result = a / b
+# result = a % b
+# result = a ** b
+result = a // b
+
+print(result)
