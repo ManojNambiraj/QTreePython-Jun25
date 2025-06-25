d7f2f7792267ba3c775b7cdd5bda57a6df77bcdc
ManojNambiraj <postboxmano@gmail.com>
2025-06-25 19:40:18 +0530

    Conditional Stmts

---
+++ class6/Statements.py
@@ -0,0 +1,38 @@
+# Control statements
+
+    # 1. Conditional statements
+            # if condition
+            # if else
+            # elif
+
+    # 2. Looping statements
+    # 3. Jumping statements
+
+# 1. Conditional statements
+            
+# if condition
+
+    # age = 15
+
+    # if(age > 18):
+    #     print("Eligible for voting")
+
+# if else
+
+    # age = 22
+
+    # if(age > 18):
+    #     print("Eligible for voting")
+    # else:
+    #     print("Not eligible")
+
+# elif
+
+age = 23
+
+if(age > 18):
+    print("Eligible for voting")
+elif(age == 18):
+    print("Partially eligible")
+else:
+    print("Not eligible")
