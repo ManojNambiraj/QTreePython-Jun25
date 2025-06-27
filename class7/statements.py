7e0719333a8b836d2129fcd2e585e6847a9efbe5
ManojNambiraj <postboxmano@gmail.com>
2025-06-27 19:12:20 +0530

    Looping & Jumping statements

---
+++ class7/statements.py
@@ -0,0 +1,54 @@
+# Control statements
+    # 1. Conditional stmts
+    # 2. Looping stmts
+    # 3. Jumping stmts
+
+# 2. Looping stmts
+
+    # for loop
+
+    # EX:1
+
+        # for i in range(3, 8):
+        #     print(i)
+
+    # EX:2
+
+        # fruits = ["apple", "banana", "orange", "watermelon"]
+
+        # for item in fruits:
+        #     print(item)
+
+    # while loop
+
+        # i = 1
+
+        # while(i <= 15):
+        #     print(i)
+        #     i += 1
+
+# 3. Jumping stmts
+
+    # Break
+
+        # i = 1
+
+        # while(i <= 15):
+
+        #     if(i == 5):
+        #         break
+
+        #     print(i)
+        #     i += 1
+
+        # Continue
+
+            # i = 1
+
+            # while(i <= 15):
+            #     i += 1
+            #     if(i == 5):
+            #         continue
+
+            #     print(i)
+    
